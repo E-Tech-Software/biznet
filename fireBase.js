f1b107d4822b0327bf98fbae95f4acd34459a00b
E-Tech-Software <etechsoftwares413@gmail.com>
2025-12-15 14:59:35 +0100

    Update fireBase.js

--- fireBase.js
+++ fireBase.js
@@ -60,10 +60,10 @@ export function add(){
 export async function root() {
     businessPathName = localStorage.getItem("path")
      console.log(businessPathName)
-        var userBucket =  await get(ref(db,"businessUsers/" + "Mavi"))
+        var userBucket =  await get(ref(db,"businessUsers/" + "Emmanuel"))
         if(userBucket.exists()){
             
-            var productBucket = await get(ref(db,"products/" + "Mavi"))
+            var productBucket = await get(ref(db,"products/" + "Emmanuel"))
             
             //load page with the data
             return{
@@ -76,6 +76,7 @@ export async function root() {
         alert("This user is not registered")
      console.log(businessPathName)
 }
+
 
 
 
